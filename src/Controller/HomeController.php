9f3a263e1947b696cab3a974e7db48ca3feab7af
Fatima <112809793+fatimazen@users.noreply.github.com>
2023-04-20 14:17:47 +0200

    correction des erreurs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/home', name: 'app_home',)]
     public function index(): Response
     {
+
+
+
+
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
