c3f6fbef7e610c7bd6124ff355d3444778aab22c
fatima <fatimazen@outlook.fr>
2023-04-26 18:41:14 +0200

    remov vitch

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
