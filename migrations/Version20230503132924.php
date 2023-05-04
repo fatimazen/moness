b28ed848f2e044c637cd6e94a13ee28ffe5c35c2
fatima <fatimazen@outlook.fr>
2023-05-04 09:37:26 +0200

    ajout page confirmation fini

--- migrations/Version20230503132924.php
+++ migrations/Version20230503132924.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230501105153 extends AbstractMigration
+final class Version20230503132924 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20230501105153 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users ADD is_subscribed TINYINT(1) NOT NULL, ADD is_verified TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE ess CHANGE phone_number phone_number VARCHAR(13) NOT NULL');
+        $this->addSql('ALTER TABLE users CHANGE is_abonne_news_letter is_abonne_news_letter TINYINT(1) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users DROP is_subscribed, DROP is_verified');
+        $this->addSql('ALTER TABLE ess CHANGE phone_number phone_number VARCHAR(20) NOT NULL');
+        $this->addSql('ALTER TABLE users CHANGE is_abonne_news_letter is_abonne_news_letter TINYINT(1) NOT NULL');
     }
 }
