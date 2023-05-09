146516d2454908ffc690889be3bbf07463500728
fatima <fatimazen@outlook.fr>
2023-05-09 17:00:50 +0200

    pagination ok adminESS

--- src/Controller/Admin/EssController.php
+++ src/Controller/Admin/EssController.php
@@ -3,17 +3,25 @@
 namespace App\Controller\Admin;
 
 use App\Repository\EssRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EssController extends AbstractController
 {
     #[Route('/admin/ess', name: 'app_admin_ess')]
-    public function index(EssRepository $repository): Response
+    public function index(EssRepository $repository,  PaginatorInterface $paginator, Request $request): Response
 
     {
-        $essS =$repository ->findAll();
+        $essS= $paginator->paginate(
+            $repository ->findAll(),
+            $request->query->getInt('page', 1), 
+            10 
+        );
+        
+        
 
 
         return $this->render('admin/ess/index.html.twig', [
