9e0b768320c8dc7e59513331159303ef5e59c4ca
fatima <fatimazen@outlook.fr>
2023-06-29 09:08:48 +0200

    mise en place distance ok

--- public/map.js
+++ public/map.js
@@ -2,9 +2,10 @@ let distance;
 let carte;
 let recherche;
 var markerClusters = L.markerClusterGroup();
-var iconBase = "image/marker.png";
+var iconBase = "/image/marker.png";
 var markers = [];
-essData = distance = "";
+var essData = { latitude: 0, longitude: 0, nameStructure: "" };
+
 
 
 carte = L.map("Carte", {
@@ -15,7 +16,7 @@ var carteTiles = L.tileLayer(
   "https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png",
   {
     attribution:
-      'données © <a href="//openstreetmap.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+      'données © <a href="//openstreetmap.org">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
     minZoom: 1,
     maxZoom: 20,
     name: "tiles",
@@ -27,12 +28,9 @@ carte.setView([46.603354, 1.888334], 6);
 new L.Control.Zoom({
   position: "topright",
 }).addTo(carte);
-var icone = L.icon({
-  iconUrl: iconBase,
-  iconSize: [50, 50],
-  iconAnchor: [25, 50],
-  popupAnchor: [0, 50],
-});
+var marker = L.marker([46.603354, 1.888334]).addTo(carte);
+
+
 
 const slideMenu = L.control.slideMenu(
   '<div id="searchBar"><input type="text" id="search-input" placeholder="Recherche..."><button id="search-button">Rechercher</button><p><label for="champ-distance">Distance : </label><input type="range" min="1" max="200" id="champ-distance"></p><p id="valeur-distance"></p></div><p id="ess"></p>'
@@ -45,7 +43,7 @@ const champDistance = document.getElementById("champ-distance");
 const valeurDistance = document.getElementById("valeur-distance");
 
 searchButton.addEventListener("click", function () {
-  const ess = searchInput.value;
+  var ess = searchInput.value;
 
   fetch(
     `https://nominatim.openstreetmap.org/search?q=${searchInput.value}&format=json&addressdetails=[0|1]&number=1polygon_svg=1`
@@ -53,62 +51,76 @@ searchButton.addEventListener("click", function () {
     .then((reponse) => reponse.json())
     .then((bldgData) => {
       console.log("bldgdata", bldgData);
-      console.log("essdata", essData);
-      console.log("bldgdata1", bldgData[0].lat);
+      console.log("ville", ess);
       let latitude = bldgData[0].lat;
       let longitude = bldgData[0].lon;
-      essData = [latitude, longitude];
-      carte.panTo(essData);
+      essData.latitude = latitude;
+      essData.longitude = longitude;
+      essData.nameStructure = ess;
+
+      carte.panTo([essData.latitude, essData.longitude]);
+
+      var marker = L.marker([essData.latitude, essData.longitude]).addTo(carte);
+      marker.bindPopup(essData.nameStructure);
+
     })
     .catch((error) => {
       console.error(error);
     });
 });
 
-champDistance.addEventListener("input", function () {
-  distance = champDistance.value;
-  valeurDistance.textContent = distance;
-});
-console.log("rare", distance);
 champDistance.addEventListener("change", function () {
-  fetch(
-    `http://127.0.0.1:8000/getEssData?latitude=${essData[0]}&longitude=${essData[1]}&distance=${distance}`
-  )
-    .then((response) => response.json())
-    .then((bldgData) => {
-      console.log(bldgData);
-      carte.eachLayer(function (layer) {
-        if (layer.options.name != "tiles") carte.removeLayer(layer);
-      });
-      // console.log("tiles",layer.options.name);
-      // On trace le cercle de rayon "distance"
-      let circle = L.circle(essData, {
-        color: '#4471C4',
-        fillColor: '#4471C4',
-        fillOpacity: 0.3,
-        radius: distance * 1000,
-      }).addTo(carte);
-      // On centre et on zoome sur le cercle
-      bounds = circle.getBounds();
-      carte.fitBounds(bounds);
 
+  distance = champDistance.value;
+  valeurDistance.textContent = distance + "Km";
 
-      // On convertit la réponse en objet Javascript
-      let donnees = JSON.parse(bldgData)
-      console.log("donner", donnees);
+  if (essData.latitude !== 0 && essData.longitude !== 0) {
 
-      Object.entries(donnees.essData).forEach(([essKey, essData]) => {
-        let marker = L.marker([essData.lat, essData.lon], {
-          icon: icone,
+    fetch(
+      `http://127.0.0.1:8000/getEssData?latitude=${essData.latitude}&longitude=${essData.longitude}&distance=${distance}`
+    )
+      .then((response) => response.json())
+      .then((bldgData) => {
+        console.log("response", bldgData);
+        // console.log("ess", essData);
+
+        carte.eachLayer(function (layer) {
+          if (layer.options.name != "tiles") carte.removeLayer(layer);
+        });
+        // On trace le cercle de rayon "distance"
+        let circle = L.circle([essData.latitude, essData.longitude], {
+          color: '#4471C4',
+          fillColor: '#4471C4',
+          fillOpacity: 0.3,
+          radius: distance * 1000,
         }).addTo(carte);
-        markerClusters.addLayer(marker);
+        var icone = L.icon({
+          iconUrl: iconBase ,
+          iconSize: [50, 50],
+          iconAnchor: [25, 50],
+          popupAnchor: [-3, -76],
+        });
+
+        Object.entries(bldgData).forEach(Element => {
+          // console.log("maker",Element[1]);
+          var marker = L.marker([Element[1].latitude, Element[1].longitude],{icon:icone});
+          marker.bindPopup(Element);
+          markerClusters.addLayer(marker); // Nous ajoutons le marqueur aux groupes
+          markers.push(marker); // Nous ajoutons le marqueur à la liste des marqueurs
+          // console.log(marker);
+        })
+        var group = new L.featureGroup(markers); // Nous créons le groupe des marqueurs pour adapter le zoom
+        carte.fitBounds(group.getBounds().pad(0.5)); // Nous demandons à ce que tous les marqueurs soient visibles, et ajoutons un padding (pad(0.5)) pour que les marqueurs ne soient pas coupés
+        carte.addLayer(markerClusters);
+        // On centre et on zoome sur le cercle
+        bounds = circle.getBounds();
+        carte.fitBounds(bounds);
+
+      })
+      .catch((error) => {
+        console.error(error);
       });
-
-
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  }
 });
 
 function ajaxGet(url) {
